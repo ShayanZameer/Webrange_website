861a2aa224e615b79a70d05c3bf282bbfa0f64a2
Shayan Zameer <alizadishah2@gmail.com>
2025-01-18 16:18:34 +0500

    CHORE:Worls map issue in production

--- company_website/src/components/global/Worldmap.jsx
+++ company_website/src/components/global/Worldmap.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import IMAGES from "@/constants/images";
+// import IMAGES from "@/constants/images";
 
 const Worldmap = () => {
   return (
     <div className="relative w-full h-64 sm:h-96 lg:h-[500px]">
       <Image
-        src={IMAGES.worldmap}
+        src="/Images/Worldmap/Worldmap.png"
         alt="World Map"
         fill
         className="object-cover"
